4523ee4a20ce2f82f0e4076a0745661351c46f80
mnguyen0226 <mnguyen0226@vt.edu>
2024-06-22 17:04:57 -0400

    Added templates

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ python main.py
 
 ## Important concepts
 - Decorator in Flask allow us to add additional functionality to the existing function. This allow us to write function for the specific page.
-- You can have multiple decorator-route for the same page
+- You can have multiple decorator-route for the same page.
+- Flask can be just be used for static-page template.
--- src/app.py
+++ src/app.py
@@ -1,17 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 # Flask know where to look for template file
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home_page():
-    return "<h1>Home Page</h1>"
+    return render_template("home.html")
 
 
 @app.route("/about")
 def about_page():
-    return "<h1>About Page</h1>"
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
