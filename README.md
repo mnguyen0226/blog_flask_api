f78627c164ad1f01c477a06bb40e2a04932290d6
mnguyen0226 <mnguyen0226@vt.edu>
2024-06-22 17:18:13 -0400

    Inserted input in templates with Jinja2

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ python main.py
 - Decorator in Flask allow us to add additional functionality to the existing function. This allow us to write function for the specific page.
 - You can have multiple decorator-route for the same page.
 - Flask can be just be used for static-page template.
+- In HTML, we can use template inheritance.
--- src/app.py
+++ src/app.py
@@ -4,16 +4,31 @@ from flask import render_template
 # Flask know where to look for template file
 app = Flask(__name__)
 
+all_posts = [
+    {
+        "author": "Minh Nguyen",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "data_posted": "June 22, 2024",
+    },
+    {
+        "author": "Binh Nguyen",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "data_posted": "June 21, 2024",
+    },
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html", posts=all_posts)
 
 
 @app.route("/about")
 def about_page():
-    return render_template("about.html")
+    return render_template("about.html", title="About Page")
 
 
 if __name__ == "__main__":
