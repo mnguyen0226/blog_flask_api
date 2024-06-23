a6214e94e1b1ac672e9ed64563dd3eaedf0bb7d0
mnguyen0226 <mnguyen0226@vt.edu>
2024-06-22 23:17:53 -0400

    Added routes and templates for forms

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from forms import RegistrationForm
+from forms import LoginForm
 
 # Flask know where to look for template file
 app = Flask(__name__)
+
+# Set a secret key for the application, which will protect from modifying cookies and cross-site request.
+app.config["SECRET_KEY"] = "c44a945cf5e0e4cbf95906f9cf4cf4a4"  # secrets lib
 
 all_posts = [
     {
@@ -30,6 +35,18 @@ def home_page():
 @app.route("/about")
 def about_page():
     return render_template("about.html", title="About Page")
+
+
+@app.route("/register")
+def register_page():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login_page():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
